641846e69910f79110e2af8d6787d4a19ae85c59
elliedelyons <gdelyons@ucsc.edu>
2023-05-15 23:03:30 -0700

    change

--- lab10/lab.js
+++ lab10/lab.js
@@ -3,42 +3,9 @@
 * Created: 11 May 2023
 * License: public domain
 */
-
-main();
-function reverseString(person){
-    return person.split(' ').reverse().join(' ')
-}
-
-function reversePerson(person){
-    return person.split(' '),reverse();
-}
-
-function fixPerson(person){
-    document.writeln("Hello + name: ",
-        fixPerson(person),"</bra>",
-        JSON,stringify(person, null, '\t'), "</pre>");
-}
-
-function main(){
-let person = prompt("Please enter your name",);
-
-    "Hello " + person + "! How are you today?";
-
-reverseString(person);
-document.writeln(reverseString(person))
-
-console.log("Main function started.");
-
-document.writeln("fixing your name..." + person + "<br>")
-
-console.log("what a beautiful name " + person + "is!")
-}
-
-
-function sayUserName () {
-var userName = window.prompt ("Hi. PLease tell me your name so I can fix it.");
-let person = prompt ("Please enter your name", "Ellie");
-console.log("userName =", Ellie);
+{
+let person = prompt ("Please enter your name");
+console.log("userName = ");
 var nameArray = userName.split('');
 console.log("nameArray=", nameArray);
 var nameArraySort = nameArray.sort();
@@ -46,13 +13,19 @@ console.log("nameArraySort =", nameArraySort);
 var nameSorted = nameArraySort.join('');
 console.log("nameSorted =", nameSorted);
 return nameSorted;
+var nameArray
 }
 
-document.writeln("Oh hey, I've fixed your name:",
-    sortUserName(), "</br>");
 
 var buttonEl = document.getElementById("my-button")
 buttonEl.addEventListener("click", function () {
     var name = prompt("What is your name?");
     var nameStr = "Hello, " + name;
+var name
+function () = anagram
+
 })
+
+
+
+
